d449a0eca775dbdb543eb9fdb0c83249c9130985
javedwasim <javedafaq@gmail.com>
2016-09-08 16:12:53 +0500

    update origin

--- resources/views/iscontacts.blade.php
+++ resources/views/iscontacts.blade.php
@@ -61,7 +61,7 @@
             </div>
         </div>
     </div>
-    <div id="dialog-confirm" title="Delete contact?">
+    <div id="dialog-confirm" title="Delete contact?" style="display: none;">
         <p><span class="ui-icon ui-icon-alert" style="float:left; margin:12px 12px 20px 0;"></span>This contact will be permanently deleted and cannot be recovered. Are you sure?</p>
     </div>
     @endsection
